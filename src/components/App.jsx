18f6d3727de39986f753b8e89bd2ed82cde858d7
maisaa <maisaa.alkhedr@gmail.com>
2017-09-07 18:06:53 +0300

    YouTup 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-var App = () => (
+ var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
@@ -7,14 +7,16 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><VideoPlayer /></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+       
+        <div><VideoList videos={exampleVideoData}/></div>
       </div>
     </div>
   </div>
 );
+ 
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
